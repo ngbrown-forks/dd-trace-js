d04a2a29705e4fda21f8babf6cb52a11b0220e76
Bryan English <bryan.english@datadoghq.com>
2022-02-13 13:56:37 -0500

    migrate knex to new plugin system

---
+++ packages/datadog-instrumentations/test/knex.spec.js
@@ -0,0 +1,36 @@
+'use strict'
+
+require('../src/knex')
+const { storage } = require('../../datadog-core')
+
+describe('Instrumentation', () => {
+  let knex
+  let client
+  const store = 'store'
+
+  describe('knex', () => {
+    withVersions('knex', 'knex', version => {
+      describe('without configuration', () => {
+        beforeEach(() => {
+          knex = require(`../../../versions/knex@${version}`).get()
+          client = knex({
+            client: 'sqlite3',
+            connection: {
+              filename: ':memory:'
+            }
+          })
+        })
+        afterEach(() => client.destroy())
+
+        it('should propagate context', () =>
+          storage.run(store, () =>
+            client.raw('PRAGMA user_version')
+              .then(() => {
+                expect(storage.getStore()).to.equal(store)
+              })
+          )
+        )
+      })
+    })
+  })
+})
