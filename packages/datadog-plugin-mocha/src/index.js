c7901ce1fc9d757a455dbe319fca3c715d27fd86
Juan Fernandez <juan.fernandezdealba@datadoghq.com>
2022-03-17 15:44:37 +0100

    add cover if empty

--- packages/datadog-plugin-mocha/src/index.js
+++ packages/datadog-plugin-mocha/src/index.js
@@ -139,7 +139,7 @@ class MochaPlugin extends Plugin {
     if (testParametersString) {
       testSpanMetadata[TEST_PARAMETERS] = testParametersString
     }
-    const codeOwners = getCodeOwnersForFilename(testSpanMetadata[TEST_SUITE])
+    const codeOwners = getCodeOwnersForFilename(testSpanMetadata[TEST_SUITE], this.codeOwnersEntries)
 
     if (codeOwners) {
       testSpanMetadata[TEST_CODE_OWNERS] = codeOwners
